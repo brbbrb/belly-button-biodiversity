36aa43d8c698af5c12b16abfa3f3859cf8999503
brbbrb <85380887+brbbrb@users.noreply.github.com>
2021-10-21 18:31:27 -0400

    after office hours

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,6 +1,11 @@
 // select dropdown menu from html
 var dropdownMenu = d3.select('#selDataset')
 
+d3.select('#selDataset').on('change', function(){
+    var chosenDropdownMenu = eval(d3.select(this).property('value'));
+    plotAllCharts(chosenDropdownMenu);
+});
+
 // select demographic info display from html
 var demographicTable = d3.select('#sample-metadata')
 
@@ -10,35 +15,50 @@ var barChart = d3.select('#bar')
 // select bubble chart from html
 var bubbleChart = d3.select('#bubble')
 
-// function optionChanged(subjectID) {
-//     console.log(subjectID)
-// }
+// 
+var allTheData;
 
+var chosenID;
+
+// initialize page
 function init() {
 
     // use d3 library to read json file
-    d3.json("samples.json").then(function(data) {
+    d3.json('samples.json').then(function (data) {
+        allTheData = data;
         data.names.forEach((name => {
             var option = dropdownMenu.append('option');
             option.text(name)
         }));
+        plotAllCharts(data.names[0]);
+    })
+    // var initID = dropdownMenu.property('value');
+    
+    // plot the charts
+    
+}
 
-        /* loop through the names using for each data.names, 
+
+// console.log(dropdownMenu).property('value');
+
+// plot all charts
+function plotAllCharts(chosenDropdownMenu) {
+    console.log(chosenDropdownMenu);
+    chosenID = chosenDropdownMenu;
+        // define metadata for chosen id
+        var metadata = allTheData.metadata;
+
+        // 
+
+}
+
+
+/* loop through the names using for each data.names, 
         then selector.append(dropdown menu)a new option.where 
         text is subject id and property value is subject id
         seperate function using d3.filter to filter "metadata" 
         to populate demographics table add a filter statement 
         to filter the samples */
-    })
-        var initID = dropdownMenu.property('value')
-
-        // plot the charts
-        plotCharts(initID)
-}
-
-
-
-
-
 
 init();
+
